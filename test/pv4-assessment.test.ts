1eb702da5e7c22baca866876ab07c9fc2f8541ec
Bruno Pasqualini <brunopasqualini.io@gmail.com>
2026-09-20 11:49:38 +0930

    cdk init

---
+++ test/pv4-assessment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Pv4Assessment from '../lib/pv4-assessment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pv4-assessment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Pv4Assessment.Pv4AssessmentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
